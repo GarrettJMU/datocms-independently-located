a8bcc6d65e29627a04f918f634114ae62673188b
garrettjmu <garretthughes3@gmail.com>
2021-01-31 15:04:37 -0800

    Add in more logs

--- components/intro.js
+++ components/intro.js
@@ -1,5 +1,5 @@
 export default function Intro({metaTags}) {
-    console.log(metaTags)
+    console.log(metaTags.filter((metaObject)=>metaObject.attributes == 'og:image'))
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
