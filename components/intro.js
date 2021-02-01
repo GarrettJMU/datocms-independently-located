51e4a1fba9817cd529ba485a183ba122c45ce1ed
garrettjmu <garretthughes3@gmail.com>
2021-01-31 19:24:24 -0800

    add in rest

--- components/intro.js
+++ components/intro.js
@@ -1,15 +1,14 @@
 export default function Intro({metaTags}) {
-    metaTags.forEach((metaObject)=>(console.log(metaObject?.property)))
-    console.log("here",metaTags.filter((metaObject)=>(metaObject?.property == 'og:image')))
-  return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Independently Located.
-      </h1>
-      <div className="text-center md:text-right text-lg mt-5 md:pl-8">
-          {/*TODO: add the logo here*/}
-          {/*<img src={}/>*/}
-      </div>
-    </section>
-  )
+    // let ogImage = metaTags.filter((metaObject) => (metaObject.attributes?.property == 'og:image'))[0]
+    return (
+        <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+
+            <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+                Independently Located.
+            </h1>
+            <div className="text-center md:text-right text-lg mt-5 md:pl-8">
+                {/*<img src={ogImage.attributes.content}/>*/}
+            s</div>
+        </section>
+    )
 }
