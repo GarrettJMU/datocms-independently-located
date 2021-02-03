1d80bdf94e076b5af2109ac0c583c7a89dd7496f
garrettjmu <garretthughes3@gmail.com>
2021-02-02 18:29:06 -0800

    add ga

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,43 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+    Html, Head, Main, NextScript,
+} from 'next/document';
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return {...initialProps};
+    }
+
+    render() {
+        const GA_MEASUREMENT_ID = 'G-R6F530KBNC';
+        return (
+            <Html lang="en">
+                <Head>
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+                    />
+                    <script
+                        // eslint-disable-next-line react/no-danger
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+                        }}
+                    />
+                </Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        );
+    }
 }
+
+export default MyDocument;
